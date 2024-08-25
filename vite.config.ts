a37ee8a70d3d463b158689a31661699e51d1f4f9
Y.DoThien <haloany123@gmail.com>
2024-08-25 15:27:07 +0700

    feat: update to gh-action

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({ mode }: ConfigEnv) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  return defineConfig({
+    plugins: [react()],
+    base: process.env.VITE_PUBLIC_URL || '/',
+  });
+}

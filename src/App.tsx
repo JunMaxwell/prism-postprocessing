bb93af96bea7c329115de21fa1b2c06076753f74
Y.DoThien <haloany123@gmail.com>
2024-08-25 15:17:02 +0700

    feat: entirety (not working tho)

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,114 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import * as THREE from "three";
+import { useRef, useCallback, useState } from "react";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { Center, Text3D } from "@react-three/drei";
+import { Bloom, EffectComposer, LUT } from "@react-three/postprocessing";
+import { LUTCubeLoader } from "postprocessing";
+import { Beam } from "./components/Beam";
+import { Rainbow } from "./components/Rainbow";
+import { Prism } from "./components/Prism";
+import { Flare } from "./components/Flare";
+import { Box } from "./components/Box";
+import { calculateRefractionAngle, lerp, lerpV3 } from "./utils";
+import { CustomShaderMaterial } from "./types/Rainbow.interfaces";
+import { ReflectApi, ReflectEvent } from "./types/Reflect.interfaces";
 
-function App() {
-	const [count, setCount] = useState(0);
+function Scene() {
+	const [isPrismHit, hitPrism] = useState(false);
+	const flare = useRef<THREE.Group>(null);
+	const ambient = useRef<THREE.AmbientLight>(null);
+	const spot = useRef<THREE.SpotLight>(null);
+	const boxreflect = useRef<ReflectApi>(null);
+	const rainbow = useRef<THREE.Mesh>(null);
+
+	const rayOut = useCallback(() => hitPrism(false), []);
+	const rayOver = useCallback((e: ReflectEvent) => {
+		if (!rainbow.current) return;
+		// Break raycast so the ray stops when it touches the prism
+		e.stopPropagation();
+		hitPrism(true);
+		// Set the intensity really high on first contact
+		const material = rainbow.current.material as CustomShaderMaterial;
+		material.speed = 1;
+		material.emissiveIntensity = 20;
+	}, []);
+
+	const vec = new THREE.Vector3();
+	const rayMove = useCallback((event: ReflectEvent) => {
+		const { api, position, direction, normal } = event;
+		if (!normal || !direction) return;
+		if (!rainbow.current || !flare.current || !spot.current) return;
+		// Extend the line to the prisms center
+		vec.toArray(api.positions, api.number++ * 3);
+		// Set flare
+		flare.current.position.set(position.x, position.y, -0.5);
+		flare.current.rotation.set(0, 0, -Math.atan2(direction.x, direction.y));
+		// Calculate refraction angles
+		let angleScreenCenter = Math.atan2(-position.y, -position.x);
+		const normalAngle = Math.atan2(normal.y, normal.x);
+		// The angle between the ray and the normal
+		const incidentAngle = angleScreenCenter - normalAngle;
+		// Calculate the refraction for the incident angle
+		const refractionAngle = calculateRefractionAngle(incidentAngle) * 6;
+		// Apply the refraction
+		angleScreenCenter += refractionAngle;
+		rainbow.current.rotation.z = angleScreenCenter;
+		// Set spot light
+		lerpV3(spot.current.target.position, [Math.cos(angleScreenCenter), Math.sin(angleScreenCenter), 0], 0.05);
+		spot.current.target.updateMatrixWorld();
+	}, []);
+
+	useFrame((state) => {
+		if (!boxreflect.current || !rainbow.current || !spot.current || !ambient.current) return;
+		// Tie beam to the mouse
+		boxreflect.current.setRay([(state.pointer.x * state.viewport.width) / 2, (state.pointer.y * state.viewport.height) / 2, 0], [0, 0, 0]);
+		// Animate rainbow intensity
+		const material = rainbow.current.material as CustomShaderMaterial;
+		lerp(material, "emissiveIntensity", isPrismHit ? 2.5 : 0, 0.1);
+		spot.current.intensity = material.emissiveIntensity;
+		// Animate ambience
+		lerp(ambient.current, "intensity", 0, 0.025);
+	});
 
 	return (
 		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			{/* Lights */}
+			<ambientLight ref={ambient} intensity={0} />
+			<pointLight position={[10, -10, 0]} intensity={0.05} />
+			<pointLight position={[0, 10, 0]} intensity={0.05} />
+			<pointLight position={[-10, 0, 0]} intensity={0.05} />
+			<spotLight ref={spot} intensity={1} distance={7} angle={1} penumbra={1} position={[0, 0, 1]} />
+			{/* Caption */}
+			<Center top bottom position={[0, 2, 0]}>
+				<Text3D size={0.7} letterSpacing={-0.05} height={0.05} font="/fonts/Inter_Bold.json">
+					Dynamic without Limits
+					<meshStandardMaterial color="white" />
+				</Text3D>
+			</Center>
+			{/* Prism + blocks + reflect beam */}
+			<Beam ref={boxreflect} bounce={10} far={20}>
+				<Prism position={[0, -0.5, 0]} onRayOver={rayOver} onRayOut={rayOut} onRayMove={rayMove} />
+				<Box position={[2.25, -3.5, 0]} rotation={[0, 0, Math.PI / 3.5]} />
+				<Box position={[-2.5, -2.5, 0]} rotation={[0, 0, Math.PI / 4]} />
+				<Box position={[-3, 1, 0]} rotation={[0, 0, Math.PI / 4]} />
+			</Beam>
+			{/* Rainbow and flares */}
+			<Rainbow ref={rainbow} startRadius={0} endRadius={0.5} fade={0} />
+			<Flare ref={flare} visible={isPrismHit} renderOrder={10} scale={1.25} streak={[12.5, 20, 1]} />
 		</>
 	);
 }
 
-export default App;
+export default function App() {
+	const texture = useLoader(LUTCubeLoader, "/lut/F-6800-STD.cube") as unknown as THREE.Texture;
+	return (
+		<Canvas orthographic gl={{ antialias: false }} camera={{ position: [0, 0, 100], zoom: 70 }}>
+			<color attach="background" args={["black"]} />
+			<Scene />
+			<EffectComposer enableNormalPass={false}>
+				<Bloom mipmapBlur levels={9} intensity={1.5} luminanceThreshold={1} luminanceSmoothing={1} />
+				<LUT lut={texture} />
+			</EffectComposer>
+		</Canvas>
+	);
+}
